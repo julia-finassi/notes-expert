9203a077146e9d42bff8ef8aa379ff2faeb599e7
Júlia Finassi <juliafinassi@outlook.com>
2024-02-11 15:02:55 +0000

    Dialogs

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -9,7 +10,7 @@ export function App() {
       <form className='w-full'>
         <input 
           type='text' 
-          placeholder='Search on your notes...' 
+          placeholder='Search in your notes...' 
           className='w-full bg-transparent outline-none text-3xl font-semibold tracking-light placeholder:text-slate-500'
         />
       </form>
@@ -19,8 +20,11 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
+
+        <NoteCard note={{
+  date: new Date(),
+  content: 'Hello World'
+}} />
 
       </div>
     </div>
